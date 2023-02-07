7e2e5fef940f4c844e4f49cac768045f824e20e3
Tobias Olsson <tobias.olsson@pm.me>
2023-02-07 10:42:01 +0100

    git ignore

---
+++ src/main/java/com/pokecardpro/PokecardproApplication.java
@@ -0,0 +1,13 @@
+package com.pokecardpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokecardproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokecardproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokecardpro/PokecardproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokecardpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokecardproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
