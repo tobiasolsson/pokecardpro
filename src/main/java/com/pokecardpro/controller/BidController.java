1df59408cd8c777b4e455d259d2b5cd042ce0a31
CodeMagnus1994 <magnusolsson1994@hotmail.se>
2023-03-20 21:26:34 +0100

    added / to bid requestmapping

--- src/main/java/com/pokecardpro/controller/BidController.java
+++ src/main/java/com/pokecardpro/controller/BidController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api")
+@RequestMapping(value = "/api/")
 public class BidController {
 
     @Autowired
